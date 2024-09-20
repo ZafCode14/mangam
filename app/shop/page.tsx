f2a2241f3e22dd697b8e0cbf3b2f1052d911b650
ZafCode14 <zafarwp14@gmail.com>
2024-09-20 15:55:22 +0300

    suspense for the shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import { Suspense } from "react";
 import Brands from "@/components/brands";
 import Products from "@/components/products";
 import { useState, useEffect } from "react";
@@ -9,26 +11,27 @@ import { useSearchParams } from "next/navigation";
 function Page() {
   const [search, setSearch] = useState<string>("");
   const [categories, setCategories] = useState<string[]>([]);
-  const [price, setPrice] = useState<number[]>([0, 300000])
+  const [price, setPrice] = useState<number[]>([0, 300000]);
   const searchParams = useSearchParams();
-  const [show, setShow] = useState<string | null>(searchParams.get('show'));
+  const [show, setShow] = useState<string | null>(searchParams.get("show"));
 
   useEffect(() => {
-    const a = searchParams.get('show');
+    const a = searchParams.get("show");
     setShow(a);
   }, [searchParams]);
 
-
   return (
     <main className={`flex justify-center w-full h-screen text-white`}>
-      <div className={`
+      <div
+        className={`
         relative top-[70px]
         flex items-start justify-center
         w-full pt-[30px] px-24
-      `} style={{
-        height: "calc(100% - 70px)"
-      }}>
-
+      `}
+        style={{
+          height: "calc(100% - 70px)",
+        }}
+      >
         <Filter
           price={price}
           categories={categories}
@@ -40,23 +43,22 @@ function Page() {
         />
 
         <div className="w-full">
-          <Search
-            search={search}
-            setSearch={setSearch}
-            show={show}
-          />
+          <Search search={search} setSearch={setSearch} show={show} />
 
-          {/** Brands or Products */}
-          {show === 'brand' ?  
-          <Brands 
-            search={search}
-          /> : 
-          <Products 
-            brandId={"all"} 
-            search={search} 
-            categories={categories} 
-            price={price} 
-            height={170}/>}
+          <Suspense fallback={<div>Loading...</div>}>
+            {/** Brands or Products */}
+            {show === "brand" ? (
+              <Brands search={search} />
+            ) : (
+              <Products
+                brandId={"all"}
+                search={search}
+                categories={categories}
+                price={price}
+                height={170}
+              />
+            )}
+          </Suspense>
         </div>
       </div>
     </main>
