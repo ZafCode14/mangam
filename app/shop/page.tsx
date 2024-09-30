aba3623f6fe62dd9f6a8ba117e04f398bf2687b4
ZafCode14 <zafarwp14@gmail.com>
2024-09-30 21:41:56 +0300

    justify end the brand

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -34,7 +34,7 @@ function ShowComponent({
   const {width} = useWindowDimensions();
 
   return (
-    <div>
+    <div className="h-full flex flex-col justify-end">
       <Search search={search} setSearch={setSearch} show={show} setShowFilter={setShowFilter}/>
       {show === "brand" ? (
         <Brands search={search} />
